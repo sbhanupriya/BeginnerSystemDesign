43f111da28d2825141505bf16ea4fa03f8155535
Bhanu Priya <sbhanupriya017@gmail.com>
2023-02-12 16:03:44 +0530

    add template for project

---
+++ src/test/java/com/beginner/systemdesign/SystemdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beginner.systemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
